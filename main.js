927108641452e4159aa8120dd556a7e80c644072
AmorosJulian <amoros.julian@hotmail.com>
2022-08-24 12:26:11 -0300

    desafio arrays

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
+// APP LISTA DE TAREAS
+// Desafio complementario Arrays
 
-for (let i = 0 ; i < 7 ; i++) {
-    let pedirPalabra = prompt(`Por favor ingrese una palabra para armar una frase`);
-    if (pedirPalabra.length >= 2 ) {
-       alert(`Palabra ingresada`);
-    } else {
-        alert(`La palabra a ingresar tiene que tener al menos 2 letras`);
+const tareas = [];
+let tarea
+
+for(i=0 ; i<=10 ; i++) {
+    if ((tareas.length <= 5) && (tarea != "")){
+        tarea = prompt(`Ingrese una nueva tarea`);
+        tareas.push(tarea);
+        console.log(tarea); 
+    } else if (tarea == "") {
+        alert(`Ingresa una tarea por favor`);
+        tarea = prompt(`Ingrese una nueva tarea`);
+        continue;
     }
-    console.log(`la palabra ingresada es ${pedirPalabra}`);
+     else {
+        alert(`Ya tenes muchas tareas pendientes`);
+        break;
+    }
+    
 }
+
+let deleteTarea = prompt(`Tareas agendadas! ¿Queres eliminar la ultima tarea? Ingrese Si o No :`);
+
+function eliminarTarea () {
+    tareas.pop();
+}
+
+if (deleteTarea === "si") {
+    eliminarTarea();
+    alert(`Ultima tarea eliminada`);
+} else {
+    alert(`Ok, tareas programadas satisfactoriamente`);
+}
+
+console.log(`Estas son las tareas programadas: ${tareas}`);
+
+
+
+
+
+
+
+
